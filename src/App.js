bfcac440faf4545e15bb62187da416e8e1c51ed3
Safdar Jamal <safdar.dev@gmail.com>
2019-08-08 21:55:30 +0500

    Update react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-
-import "./style.css";
-
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
